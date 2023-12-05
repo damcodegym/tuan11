8bef9bf000a114c8f178f026f63fac2acfb21378
bangdv <123456a@>
2023-12-05 10:52:50 +0700

    update

---
+++ maytinh.js
@@ -0,0 +1,14 @@
+
+function Sub() {
+   let number1 = document.getElementById("num1").value;
+   let number2 = document.getElementById("num2").value; 
+   let result = Number(number1) - Number(number2);
+   document.getElementById("result").innerHTML = result;
+}
+function Add() {
+   let number1 = document.getElementById("num1").value; 
+   let number2 = document.getElementById("num2").value;
+   let result = Number(number1) + Number(number2);
+   document.getElementById("result").innerHTML = result;
+}
+
